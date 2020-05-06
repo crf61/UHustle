9ced4d91033281044fc345ab5d5fb942f36b991d
crf61 <crf61@georgetown.edu>
2020-05-06 01:37:15 -0400

    Last step, have the program print out multiple items in the list

--- marketplace.py
+++ marketplace.py
@@ -11,23 +11,23 @@ if __name__ == "__main__":
     listings = [
         {"id":1, "name": "DJ Service", "school": "Georgetown University", "category": "Entertainment", "Product or Service": "Service", "price": 12.50},
         {"id":2, "name": "StrawberryCheesecake", "school": "George Washington University", "category": "Food/Drink", "Product or Service": "Product", "price": 9.99},
-        {"id":2, "name": "Strawberry Cheesecake", "school": "George Washington University", "category": "Food and Drink", "Product or Service": "Product", "price": 9.99},
-        {"id":3, "name": "Photoshoot Session", "school": "American University", "category": "Media", "Product or Service": "Service", "price": 15.00},
-        {"id":4, "name": "Party Planner", "school": "Georgetown University", "category": "Entertainment", "Product or Service": "Service", "price": 7.50},
-        {"id":5, "name": "Eyebrow Waxing", "school": "George Washington University", "category": "Beauty", "Product or Service": "Service", "price": 11.99},
-        {"id":6, "name": "Books", "school": "American University", "category": "Entertainment", "Product or Service": "Product", "price": 12.10},
-        {"id":7, "name": "Fried Conch Dinner", "school": "Georgetown University", "category": "Food/Drink", "Product or Service": "Product", "price": 4.99},
-        {"id":8, "name": "Graphic Design: Logo", "school": "American University", "category": "Media", "Product or Service": "Service", "price": 3.00},
-        {"id":9, "name": "Videography Session", "school": "George Washington University", "category": "Media", "Product or Service": "Service", "price": 12.00},
-        {"id":10, "name": "Wash and Set", "school": "American University", "category": "Beauty", "Product or Service": "Service", "price": 30.00},
-        {"id":11, "name": "Dance Performance", "school": "George Washington University", "category": "Entertainment", "Product or Service": "Service", "price": 23.99},
-        {"id":12, "name": "Trail Mix", "school": "American University", "category": "Food and Drink", "Product or Service": "Product", "price": 10.45},
-        {"id":13, "name": "Private Chef/Caterer", "school": "George Washington University", "category": "Food and Drink", "Product or Service": "Service", "price": 17.89},
-        {"id":14, "name": "Model Headshots", "school": "Georgetown University", "category": "Media", "Product or Service": "Service", "price": 5.00},
-        {"id":15, "name": "Band Performance", "school": "George Washington University", "category": "Entertainment", "Product or Service": "Service", "price": 9.49},
-        {"id":16, "name": "Cotton Candy", "school": "American University", "category": "Food and Drink", "Product or Service": "Product", "price": 5.30},
-        {"id":17, "name": "Tresure Hunt Set Up", "school": "Georgetown University", "category": "Entertainment", "Product or Service": "Service", "price": 2.99},
-        {"id":18, "name": "Facial", "school": "Georgetown University", "category": "Beauty", "Product or Service": "Service", "price": 74.99},
+        {"id":3, "name": "Strawberry Cheesecake", "school": "George Washington University", "category": "Food and Drink", "Product or Service": "Product", "price": 9.99},
+        {"id":4, "name": "Photoshoot Session", "school": "American University", "category": "Media", "Product or Service": "Service", "price": 15.00},
+        {"id":5, "name": "Party Planner", "school": "Georgetown University", "category": "Entertainment", "Product or Service": "Service", "price": 7.50},
+        {"id":6, "name": "Eyebrow Waxing", "school": "George Washington University", "category": "Beauty", "Product or Service": "Service", "price": 11.99},
+        {"id":7, "name": "Books", "school": "American University", "category": "Entertainment", "Product or Service": "Product", "price": 12.10},
+        {"id":8, "name": "Fried Conch Dinner", "school": "Georgetown University", "category": "Food/Drink", "Product or Service": "Product", "price": 4.99},
+        {"id":9, "name": "Graphic Design: Logo", "school": "American University", "category": "Media", "Product or Service": "Service", "price": 3.00},
+        {"id":10, "name": "Videography Session", "school": "George Washington University", "category": "Media", "Product or Service": "Service", "price": 12.00},
+        {"id":11, "name": "Wash and Set", "school": "American University", "category": "Beauty", "Product or Service": "Service", "price": 30.00},
+        {"id":12, "name": "Dance Performance", "school": "George Washington University", "category": "Entertainment", "Product or Service": "Service", "price": 23.99},
+        {"id":13, "name": "Trail Mix", "school": "American University", "category": "Food and Drink", "Product or Service": "Product", "price": 10.45},
+        {"id":14, "name": "Private Chef/Caterer", "school": "George Washington University", "category": "Food and Drink", "Product or Service": "Service", "price": 17.89},
+        {"id":15, "name": "Model Headshots", "school": "Georgetown University", "category": "Media", "Product or Service": "Service", "price": 5.00},
+        {"id":16, "name": "Band Performance", "school": "George Washington University", "category": "Entertainment", "Product or Service": "Service", "price": 9.49},
+        {"id":17, "name": "Cotton Candy", "school": "American University", "category": "Food and Drink", "Product or Service": "Product", "price": 5.30},
+        {"id":18, "name": "Tresure Hunt Set Up", "school": "Georgetown University", "category": "Entertainment", "Product or Service": "Service", "price": 2.99},
+        {"id":19, "name": "Facial", "school": "Georgetown University", "category": "Beauty", "Product or Service": "Service", "price": 74.99},
 
         ] # based on listing from UHustle's beta test in 2019
 
@@ -38,6 +38,7 @@ if __name__ == "__main__":
     total_price = 0
     selected_schools = []
     selected_categories = []
+    selected_ids = []
 
     Name = input("Enter your name: ")
   
@@ -54,18 +55,18 @@ if __name__ == "__main__":
                 print("SELECTED LISTINGS: " + matching_selection["name"] + " " + str(matching_selection["id"]))  
    
     while True:
-            selected_category = input("Choose from the available listings from the ids listed above: ") 
-            if selected_category == "DONE":
+            selected_id = input("Choose from the available listings from the ids listed above: ") 
+            if selected_id == "DONE":
                     break
             else: 
-                    selected_categories.append(selected_category) 
+                    selected_ids.append(selected_id) 
 
 
     import datetime
     x = datetime.datetime.now ()
 
-    for selected_category in selected_categories:
-                matching_listings = [p for p in listings if str(p["category"]) == str(selected_category)]
+    for selected_id in selected_ids:
+                matching_listings = [p for p in listings if str(p["id"]) == str(selected_id)]
                 matching_listing = matching_listings[0]
                 total_price = total_price + matching_listing["price"]
                 print("SELECTED LISTINGS: " + matching_listing["name"] + " " + str(matching_listing["price"]))  
