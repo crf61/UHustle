7be8bbf37c8854c47c00b591ccd0a4a7f7295fcf
crf61 <crf61@georgetown.edu>
2020-05-06 02:49:08 -0400

    Update marketplace.py

--- marketplace.py
+++ marketplace.py
@@ -10,13 +10,13 @@ def to_usd(my_price):
 if __name__ == "__main__":
     listings = [
         {"id":1, "name": "DJ Service", "school": "Georgetown University", "category": "Entertainment", "Product or Service": "Service", "price": 12.50},
-        {"id":2, "name": "StrawberryCheesecake", "school": "George Washington University", "category": "Food/Drink", "Product or Service": "Product", "price": 9.99},
-        {"id":3, "name": "Strawberry Cheesecake", "school": "George Washington University", "category": "Food and Drink", "Product or Service": "Product", "price": 9.99},
+        {"id":2, "name": "Strawberry Cheesecake", "school": "Georgetown University", "category": "Food and Drink", "Product or Service": "Product", "price": 9.99},
+        {"id":3, "name": "Peach Candy", "school": "George Washington University", "category": "Food and Drink", "Product or Service": "Product", "price": 9.99},
         {"id":4, "name": "Photoshoot Session", "school": "American University", "category": "Media", "Product or Service": "Service", "price": 15.00},
         {"id":5, "name": "Party Planner", "school": "Georgetown University", "category": "Entertainment", "Product or Service": "Service", "price": 7.50},
         {"id":6, "name": "Eyebrow Waxing", "school": "George Washington University", "category": "Beauty", "Product or Service": "Service", "price": 11.99},
         {"id":7, "name": "Books", "school": "American University", "category": "Entertainment", "Product or Service": "Product", "price": 12.10},
-        {"id":8, "name": "Fried Conch Dinner", "school": "Georgetown University", "category": "Food/Drink", "Product or Service": "Product", "price": 4.99},
+        {"id":8, "name": "Fried Conch Dinner", "school": "Georgetown University", "category": "Food and Drink", "Product or Service": "Product", "price": 4.99},
         {"id":9, "name": "Graphic Design: Logo", "school": "American University", "category": "Media", "Product or Service": "Service", "price": 3.00},
         {"id":10, "name": "Videography Session", "school": "George Washington University", "category": "Media", "Product or Service": "Service", "price": 12.00},
         {"id":11, "name": "Wash and Set", "school": "American University", "category": "Beauty", "Product or Service": "Service", "price": 30.00},
@@ -27,9 +27,18 @@ if __name__ == "__main__":
         {"id":16, "name": "Band Performance", "school": "George Washington University", "category": "Entertainment", "Product or Service": "Service", "price": 9.49},
         {"id":17, "name": "Cotton Candy", "school": "American University", "category": "Food and Drink", "Product or Service": "Product", "price": 5.30},
         {"id":18, "name": "Tresure Hunt Set Up", "school": "Georgetown University", "category": "Entertainment", "Product or Service": "Service", "price": 2.99},
+        
+        ] # based on listing from UHustle's beta test in 2019
+
+    Georgetown_listings = [
+        {"id":1, "name": "DJ Service", "school": "Georgetown University", "category": "Entertainment", "Product or Service": "Service", "price": 12.50},
+        {"id":5, "name": "Party Planner", "school": "Georgetown University", "category": "Entertainment", "Product or Service": "Service", "price": 7.50},
+        {"id":8, "name": "Fried Conch Dinner", "school": "Georgetown University", "category": "Food/Drink", "Product or Service": "Product", "price": 4.99},
+        {"id":15, "name": "Model Headshots", "school": "Georgetown University", "category": "Media", "Product or Service": "Service", "price": 5.00},
+        {"id":18, "name": "Tresure Hunt Set Up", "school": "Georgetown University", "category": "Entertainment", "Product or Service": "Service", "price": 2.99},
         {"id":19, "name": "Facial", "school": "Georgetown University", "category": "Beauty", "Product or Service": "Service", "price": 74.99},
 
-        ] # based on listing from UHustle's beta test in 2019
+    ]
 
    
     #
@@ -44,6 +53,10 @@ if __name__ == "__main__":
   
     while True:
             selected_school = input("Choose from the available schools: Georgetown University, George Washington University, and American University: ") 
+            if selected_school == "Georgetown University":
+                matching_schools = [p for p in Georgetown_listings if str(p["school"]) == "Georgetown University"]
+                matching_school = matching_schools[0]
+                print("Georgetown Listings: " + matching_school["name"] + " " + str(matching_school["id"]))
             if selected_school == "DONE":
                     break
             else: 
